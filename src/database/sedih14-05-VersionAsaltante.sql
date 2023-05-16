42add7b431370d98d03b624e66a2d4dbae92ad1d
josecar2505 <jogicarrilloru@ittepic.edu.mx>
2023-05-15 20:49:17 -0700

    BD QUE SI JALA
    YA CASI YA CASI

--- src/database/sedih14-05-VersionAsaltante.sql
+++ src/database/sedih14-05-VersionAsaltante.sql
@@ -29,151 +29,6 @@ SET
 --
 -- Base de datos: `sedih`
 --
-DELIMITER $ $ --
--- Procedimientos
---
-CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_diario` (IN `Hotelid` INT) BEGIN DECLARE idTipoArray TEXT;
-
-DECLARE idTipo1 TEXT;
-
-DECLARE nivelGeneral REAL DEFAULT 0;
-
-DECLARE pos INT DEFAULT 1;
-
-DECLARE total_rows INT DEFAULT 0;
-
-DECLARE nivelTipo REAL DEFAULT 0;
-
-DECLARE varCategoria INT DEFAULT 0;
-
-DECLARE varCostoP DOUBLE DEFAULT 0;
-
-SET
-    idTipoArray = (
-        SELECT
-            GROUP_CONCAT(DISTINCT habitacion.idTipo SEPARATOR ',')
-        FROM
-            habitacion
-        WHERE
-            idHotel = Hotelid
-    );
-
-SET
-    nivelGeneral = (
-        SELECT
-            ocupacion
-        FROM
-            hotel
-        WHERE
-            idHotel = Hotelid
-    );
-
-SELECT
-    COUNT(*) INTO total_rows
-FROM
-    hotel
-WHERE
-    idHotel = Hotelid;
-
-SET
-    varCategoria = (
-        SELECT
-            categoria
-        FROM
-            hotel
-        WHERE
-            idHotel = Hotelid
-    );
-
-WHILE pos <= LENGTH(idTipoArray) - LENGTH(REPLACE(idTipoArray, ',', '')) + 1 DO
-SET
-    idTipo1 = SUBSTRING_INDEX(SUBSTRING_INDEX(idTipoArray, ',', pos), ',', -1);
-
-SET
-    varCostoP =(
-        SELECT
-            AVG(costo)
-        FROM
-            habitacion
-        WHERE
-            idHotel = Hotelid
-            AND idTipo = idTipo1
-    );
-
--- SELECT idTipo1;
--- SELECT pos;
-SET
-    nivelTipo = (
-        100 *(
-            SELECT
-                COUNT(*)
-            FROM
-                habitacion
-            WHERE
-                idHotel = Hotelid
-                AND idTipo = idTipo1
-                AND estado = "OCUPADO"
-        )
-    ) /(
-        SELECT
-            COUNT(*)
-        FROM
-            habitacion
-        WHERE
-            idHotel = Hotelid
-            AND idTipo = idTipo1
-    );
-
-IF idTipo1 <> '' THEN
-INSERT INTO
-    registroocupacion
-VALUES
-    (
-        Hotelid,
-        NOW(),
-        nivelGeneral,
-        idTipo1,
-        nivelTipo,
-        varCategoria,
-        varCostoP
-    );
-
-END IF;
-
-SET
-    pos = pos + 1;
-
-END WHILE;
-
-END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_inicioSemana` (
-    IN `fechaConsulta` DATETIME,
-    OUT `SALIDA` DATETIME
-) BEGIN DECLARE SALIDA DATE;
-
-SELECT
-    DATE_SUB(DATE(NOW()), INTERVAL WEEKDAY(fechaConsulta) DAY) INTO SALIDA;
-
-END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_porTipoDeHabitacion` (IN `HotelID` INT) BEGIN
-SELECT
-    idTipo,
-    COUNT(*) as cantidad
-FROM
-    habitacion
-WHERE
-    HotelID = idHotel
-    AND estado = "OCUPADO"
-GROUP BY
-    idTipo;
-
-END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_registrocliente` (IN `varIdRegistro` INT) BEGIN
-SELECT
-    idHabitacion
-FROM
-    registrocliente
-WHERE
-    idRegistro = varIdRegistro;
-
-END $ $ DELIMITER;
 
 -- --------------------------------------------------------
 --
@@ -213,37 +68,7 @@ VALUES
     ('La-Loma-10', 10, 2, 'DISPONIBLE', 2600, 1),
     ('Las-Palomas-01', 1, 4, 'DISPONIBLE', 4600, 2);
 
---
--- Disparadores `habitacion`
---
-DELIMITER $ $ CREATE TRIGGER `tr_ocupacionDisponible` BEFORE
-UPDATE
-    ON `habitacion` FOR EACH ROW BEGIN DECLARE porcentaje REAL;
 
-SET
-    porcentaje = 0;
-
-IF NEW.estado = 'DISPONIBLE' THEN
-SET
-    porcentaje = 100 / (
-        SELECT
-            noHabitaciones
-        FROM
-            hotel
-        WHERE
-            idHotel = NEW.idHotel
-    );
-
-END IF;
-
-UPDATE
-    hotel
-SET
-    ocupacion = ocupacion - porcentaje
-WHERE
-    idHotel = NEW.idHotel;
-
-END $ $ DELIMITER;
 
 -- --------------------------------------------------------
 --
@@ -933,51 +758,7 @@ VALUES
         1
     );
 
---
--- Disparadores `registrocliente`
---
-DELIMITER $ $ CREATE TRIGGER `tr_actualizarOcupacion`
-AFTER
-INSERT
-    ON `registrocliente` FOR EACH ROW BEGIN DECLARE hotelID TEXT;
 
-DECLARE porcentaje REAL;
-
-SET
-    hotelID = (
-        SELECT
-            idHotel
-        FROM
-            habitacion
-        WHERE
-            codigoHabitacion = (
-                SELECT
-                    idHabitacion
-                FROM
-                    registrocliente
-                WHERE
-                    idRegistro = NEW.idRegistro
-            )
-    );
-
-SET
-    porcentaje = (100) /(
-        SELECT
-            noHabitaciones
-        FROM
-            hotel
-        WHERE
-            idHotel = hotelID
-    );
-
-UPDATE
-    hotel
-set
-    ocupacion = ocupacion + porcentaje
-WHERE
-    idHotel = hotelID;
-
-END $ $ DELIMITER;
 
 -- --------------------------------------------------------
 --
@@ -1193,6 +974,376 @@ ADD
     CONSTRAINT `fk_usuarios_hotel` FOREIGN KEY (`idHotel`) REFERENCES `hotel` (`idHotel`);
 
 COMMIT;
+
+DELIMITER $ $ --
+-- Procedimientos
+--
+CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_diario` (IN `Hotelid` INT) BEGIN DECLARE idTipoArray TEXT;
+
+DECLARE idTipo1 TEXT;
+
+DECLARE nivelGeneral REAL DEFAULT 0;
+
+DECLARE pos INT DEFAULT 1;
+
+DECLARE total_rows INT DEFAULT 0;
+
+DECLARE nivelTipo REAL DEFAULT 0;
+
+DECLARE varCategoria INT DEFAULT 0;
+
+DECLARE varCostoP DOUBLE DEFAULT 0;
+
+SET
+    idTipoArray = (
+        SELECT
+            GROUP_CONCAT(DISTINCT habitacion.idTipo SEPARATOR ',')
+        FROM
+            habitacion
+        WHERE
+            idHotel = Hotelid
+    );
+
+SET
+    nivelGeneral = (
+        SELECT
+            ocupacion
+        FROM
+            hotel
+        WHERE
+            idHotel = Hotelid
+    );
+
+SELECT
+    COUNT(*) INTO total_rows
+FROM
+    hotel
+WHERE
+    idHotel = Hotelid;
+
+SET
+    varCategoria = (
+        SELECT
+            categoria
+        FROM
+            hotel
+        WHERE
+            idHotel = Hotelid
+    );
+
+WHILE pos <= LENGTH(idTipoArray) - LENGTH(REPLACE(idTipoArray, ',', '')) + 1 DO
+SET
+    idTipo1 = SUBSTRING_INDEX(SUBSTRING_INDEX(idTipoArray, ',', pos), ',', -1);
+
+SET
+    varCostoP =(
+        SELECT
+            AVG(costo)
+        FROM
+            habitacion
+        WHERE
+            idHotel = Hotelid
+            AND idTipo = idTipo1
+    );
+
+-- SELECT idTipo1;
+-- SELECT pos;
+SET
+    nivelTipo = (
+        100 *(
+            SELECT
+                COUNT(*)
+            FROM
+                habitacion
+            WHERE
+                idHotel = Hotelid
+                AND idTipo = idTipo1
+                AND estado = "OCUPADO"
+        )
+    ) /(
+        SELECT
+            COUNT(*)
+        FROM
+            habitacion
+        WHERE
+            idHotel = Hotelid
+            AND idTipo = idTipo1
+    );
+
+IF idTipo1 <> '' THEN
+INSERT INTO
+    registroocupacion
+VALUES
+    (
+        Hotelid,
+        NOW(),
+        nivelGeneral,
+        idTipo1,
+        nivelTipo,
+        varCategoria,
+        varCostoP
+    );
+
+END IF;
+
+SET
+    pos = pos + 1;
+
+END WHILE;
+
+END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_inicioSemana` (
+    IN `fechaConsulta` DATETIME,
+    OUT `SALIDA` DATETIME
+) BEGIN DECLARE SALIDA DATE;
+
+SELECT
+    DATE_SUB(DATE(NOW()), INTERVAL WEEKDAY(fechaConsulta) DAY) INTO SALIDA;
+
+END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_porTipoDeHabitacion` (IN `HotelID` INT) BEGIN
+SELECT
+    idTipo,
+    COUNT(*) as cantidad
+FROM
+    habitacion
+WHERE
+    HotelID = idHotel
+    AND estado = "OCUPADO"
+GROUP BY
+    idTipo;
+
+END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_registrocliente` (IN `varIdRegistro` INT) BEGIN
+SELECT
+    idHabitacion
+FROM
+    registrocliente
+WHERE
+    idRegistro = varIdRegistro;
+
+END $ $ DELIMITER;
+
+--
+-- Disparadores `habitacion`
+--
+DELIMITER $ $ CREATE TRIGGER `tr_ocupacionDisponible` BEFORE
+UPDATE
+    ON `habitacion` FOR EACH ROW BEGIN DECLARE porcentaje REAL;
+
+SET
+    porcentaje = 0;
+
+IF NEW.estado = 'DISPONIBLE' THEN
+SET
+    porcentaje = 100 / (
+        SELECT
+            noHabitaciones
+        FROM
+            hotel
+        WHERE
+            idHotel = NEW.idHotel
+    );
+
+END IF;
+
+UPDATE
+    hotel
+SET
+    ocupacion = ocupacion - porcentaje
+WHERE
+    idHotel = NEW.idHotel;
+
+END $ $ DELIMITER;
+
+--
+-- Disparadores `registrocliente`
+--
+DELIMITER $ $ CREATE TRIGGER `tr_actualizarOcupacion`
+AFTER
+INSERT
+    ON `registrocliente` FOR EACH ROW BEGIN DECLARE hotelID TEXT;
+
+DECLARE porcentaje REAL;
+
+SET
+    hotelID = (
+        SELECT
+            idHotel
+        FROM
+            habitacion
+        WHERE
+            codigoHabitacion = (
+                SELECT
+                    idHabitacion
+                FROM
+                    registrocliente
+                WHERE
+                    idRegistro = NEW.idRegistro
+            )
+    );
+
+SET
+    porcentaje = (100) /(
+        SELECT
+            noHabitaciones
+        FROM
+            hotel
+        WHERE
+            idHotel = hotelID
+    );
+
+UPDATE
+    hotel
+set
+    ocupacion = ocupacion + porcentaje
+WHERE
+    idHotel = hotelID;
+
+END $ $ DELIMITER;
+
+DELIMITER $ $ --
+-- Procedimientos
+--
+CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_diario` (IN `Hotelid` INT) BEGIN DECLARE idTipoArray TEXT;
+
+DECLARE idTipo1 TEXT;
+
+DECLARE nivelGeneral REAL DEFAULT 0;
+
+DECLARE pos INT DEFAULT 1;
+
+DECLARE total_rows INT DEFAULT 0;
+
+DECLARE nivelTipo REAL DEFAULT 0;
+
+DECLARE varCategoria INT DEFAULT 0;
+
+DECLARE varCostoP DOUBLE DEFAULT 0;
+
+SET
+    idTipoArray = (
+        SELECT
+            GROUP_CONCAT(DISTINCT habitacion.idTipo SEPARATOR ',')
+        FROM
+            habitacion
+        WHERE
+            idHotel = Hotelid
+    );
+
+SET
+    nivelGeneral = (
+        SELECT
+            ocupacion
+        FROM
+            hotel
+        WHERE
+            idHotel = Hotelid
+    );
+
+SELECT
+    COUNT(*) INTO total_rows
+FROM
+    hotel
+WHERE
+    idHotel = Hotelid;
+
+SET
+    varCategoria = (
+        SELECT
+            categoria
+        FROM
+            hotel
+        WHERE
+            idHotel = Hotelid
+    );
+
+WHILE pos <= LENGTH(idTipoArray) - LENGTH(REPLACE(idTipoArray, ',', '')) + 1 DO
+SET
+    idTipo1 = SUBSTRING_INDEX(SUBSTRING_INDEX(idTipoArray, ',', pos), ',', -1);
+
+SET
+    varCostoP =(
+        SELECT
+            AVG(costo)
+        FROM
+            habitacion
+        WHERE
+            idHotel = Hotelid
+            AND idTipo = idTipo1
+    );
+
+-- SELECT idTipo1;
+-- SELECT pos;
+SET
+    nivelTipo = (
+        100 *(
+            SELECT
+                COUNT(*)
+            FROM
+                habitacion
+            WHERE
+                idHotel = Hotelid
+                AND idTipo = idTipo1
+                AND estado = "OCUPADO"
+        )
+    ) /(
+        SELECT
+            COUNT(*)
+        FROM
+            habitacion
+        WHERE
+            idHotel = Hotelid
+            AND idTipo = idTipo1
+    );
+
+IF idTipo1 <> '' THEN
+INSERT INTO
+    registroocupacion
+VALUES
+    (
+        Hotelid,
+        NOW(),
+        nivelGeneral,
+        idTipo1,
+        nivelTipo,
+        varCategoria,
+        varCostoP
+    );
+
+END IF;
+
+SET
+    pos = pos + 1;
+
+END WHILE;
+
+END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_inicioSemana` (
+    IN `fechaConsulta` DATETIME,
+    OUT `SALIDA` DATETIME
+) BEGIN DECLARE SALIDA DATE;
+
+SELECT
+    DATE_SUB(DATE(NOW()), INTERVAL WEEKDAY(fechaConsulta) DAY) INTO SALIDA;
+
+END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_porTipoDeHabitacion` (IN `HotelID` INT) BEGIN
+SELECT
+    idTipo,
+    COUNT(*) as cantidad
+FROM
+    habitacion
+WHERE
+    HotelID = idHotel
+    AND estado = "OCUPADO"
+GROUP BY
+    idTipo;
+
+END $ $ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_registrocliente` (IN `varIdRegistro` INT) BEGIN
+SELECT
+    idHabitacion
+FROM
+    registrocliente
+WHERE
+    idRegistro = varIdRegistro;
+
+END $ $ DELIMITER;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
 ;
